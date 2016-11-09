893e3357c352404c0960a25cf99fd4f5fa065341
Jongilizwe Gqozo <JongilizweG@jse.co.za>
2016-11-09 19:08:48 +0200

    Adding the File processor.

---
+++ CSVApp/FileProcessor/FileProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileProcessor
+{
+    public class FileProcessor : IFileProcessor
+    {
+        private readonly string _inputFileLocation;
+        private readonly string _outputFileLocation;
+
+        public FileProcessor(string inputFileLocation, string outputFileLocation)
+        {
+            _inputFileLocation = inputFileLocation;
+            _outputFileLocation = outputFileLocation;
+        }
+
+        public void FileReader()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OutPutFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProcessFile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
